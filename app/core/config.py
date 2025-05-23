35d137ba920a4a05e5eb73b3734d04edb7b2ae12
Piyush Mohapatra <piyush.ofcl@gmail.com>
2025-05-23 06:53:22 +0530

    Refactor security settings in config.py to use environment variables for SECRET_KEY, ALGORITHM, and ACCESS_TOKEN_EXPIRE_MINUTES

--- app/core/config.py
+++ app/core/config.py
@@ -10,9 +10,9 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api"
     
     # Security
-    SECRET_KEY: str = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    SECRET_KEY: str = os.environ["SECRET_KEY"]
+    ALGORITHM: str = os.environ["ALGORITHM"]
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.environ["ACCESS_TOKEN_EXPIRE_MINUTES"])
     
     # Database
     DATABASE_URL: str = os.environ["DATABASE_URL"]
